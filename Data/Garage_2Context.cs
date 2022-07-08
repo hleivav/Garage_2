1192c16d2a4f56e0131e06534e0ced7308bf4f5c
Patrick Malmkvist <Patrick.malmkvist@outlook.com>
2022-07-08 15:18:12 +0200

    database, lagt till bilarna

--- Data/Garage_2Context.cs
+++ Data/Garage_2Context.cs
@@ -15,5 +15,33 @@ namespace Garage_2.Data
         }
 
         public DbSet<Garage_2.Models.Vehicles>? Vehicles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+
+
+            modelBuilder.Entity<Vehicles>().HasData(
+
+                new Vehicles { Id = 1, RegNo = "MDK626", VehicleType = VehicleType.Car, Color = Color.White, Make = "Volvo", Model = "V70", NoOfWheels = 4, PartkingStartAt = DateTime.Now},
+
+                    new Vehicles { Id = 2, RegNo = "TTT234", VehicleType = VehicleType.Car, Color = Color.Blue, Make = "tesla", Model = "D3", NoOfWheels = 4, PartkingStartAt = DateTime.Now },
+
+                    new Vehicles { Id = 3, RegNo = "ABC123", VehicleType = VehicleType.Car, Color = Color.Black, Make = "mercedes", Model = "C-Klass", NoOfWheels = 4, PartkingStartAt = DateTime.Now },
+
+                    new Vehicles { Id = 4, RegNo = "DUM666", VehicleType = VehicleType.Bus, Color = Color.Yellow, Make = "Volvo", Model = "Volvobuss", NoOfWheels = 8, PartkingStartAt = DateTime.Now },
+
+                    new Vehicles { Id = 5, RegNo = "MUU775", VehicleType = VehicleType.Boat, Color = Color.Black, Make = "Princess", Model = "Yatch", NoOfWheels = 0, PartkingStartAt = DateTime.Now },
+
+                    new Vehicles { Id = 6, RegNo = "YUU223", VehicleType = VehicleType.Car, Color = Color.Red, Make = "Volvo", Model = "V90", NoOfWheels = 4, PartkingStartAt = DateTime.Now }
+
+
+                ); ; 
+
+        }
     }
+
 }
--- Migrations/20220708130515_Seed.Designer.cs
+++ Migrations/20220708130515_Seed.Designer.cs
@@ -4,6 +4,7 @@ using Garage_2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Garage_2.Migrations
 {
     [DbContext(typeof(Garage_2Context))]
-    partial class Garage_2ContextModelSnapshot : ModelSnapshot
+    [Migration("20220708130515_Seed")]
+    partial class Seed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
