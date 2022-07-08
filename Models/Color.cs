e5ff9a629d17e1551d545a29395ded36e7ca0b1a
Patrick Malmkvist <Patrick.malmkvist@outlook.com>
2022-07-08 11:57:00 +0200

    Add enums

---
+++ Models/Color.cs
@@ -0,0 +1,12 @@
+﻿namespace Garage_2.Models
+{
+    public enum Color
+    {
+        red, 
+        yellow,
+        blue,
+        black,
+    }
+
+
+}
---
+++ Models/VehicleType.cs
@@ -0,0 +1,11 @@
+﻿namespace Garage_2.Models
+{
+    public enum VehicleType
+    {
+        Car,
+        Boat,
+        Buss,
+    }
+
+
+}
