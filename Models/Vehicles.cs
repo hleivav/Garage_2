f8a29af22c384202553676aaaed3569f64e843f4
hleivav <hleiva@hotmail.com>
2022-07-08 10:52:22 +0200

    vehicle start

---
+++ Models/Vehicles.cs
@@ -0,0 +1,10 @@
+﻿namespace Garage_2.Models
+{
+    public class Vehicles
+    {
+        public enum vehicle { get; set; }
+        public string RegNo { get; set; }
+        public enum Color { get; set; }
+        public string MyProperty { get; set; }
+    }
+}
